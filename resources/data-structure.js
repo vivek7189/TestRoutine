38b46d7a9f391ed34d1f3c5f5a0fa79888877a48
Vivek Kumar <vivek.kumar1@markitserv.com>
2016-06-13 16:33:57 +0530

    data structure in javascript (linkedList)

--- resources/data-structure.js
+++ resources/data-structure.js
@@ -24,6 +24,48 @@ Linkedlist.prototype = {
            
         }
          this.length++;
+    },
+
+    remove : function(value){
+             if(this.root.value == value){
+                    this.root= this.root.next;
+                    this.length--;
+             }else{
+                 var current=this.root;
+                 var previous;
+                 while(current.value != value){
+                        previous=current;
+                        current = current.next;
+                        var temp=current;
+                        console.log(current);
+                        count++;
+                 }
+                 previous.next=current.next
+                // current = current.next;
+                
+                // delete node.value;
+                 this.length--;
+             }
+    },
+
+    find : function (value){
+        if(this.root == value){
+                return true;
+        }else {
+            var node = this.root;
+            var count=0;
+           
+            while(node.value !== value){
+                    node = node.next;
+                    console.log('node',node);
+                    console.log(++count,node);
+            }
+            if(this.length >count){
+
+            }
+            return node;
+            
+        }
     }
 }
 
@@ -31,5 +73,10 @@ var ls= new Linkedlist();
 ls.add(8);
 ls.add(9);
 ls.add(91);
+ls.add(99);
 ls.add(910);
-console.log(ls);
+ls.add(1000);
+
+
+
+console.log(ls.find(910));
