a095b07ded3ebc6d241a6a257e57aba9ee6675bd
Vivek Kumar <vivek.kumar1@markitserv.com>
2016-07-20 16:55:35 +0530

    'crawler'

--- server.js
+++ server.js
@@ -3,26 +3,24 @@
  */
 
  var express = require('express');
+ var streamBuffers = require('stream-buffers');
  var app = express();
+ var crawler = require('js-crawler');
  app.use('/',express.static('./'));
  app.use('/boot', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 // now start the server
 
 var server = app.listen(8000,function (){
-    console.log('Listening on port :',server.address().port);
+     console.log('Listening on port :',server.address().port);
 })
+var data='';
+new crawler().configure({depth: 2})
+  .crawl("http://google.com", function onSuccess(page) {
+        data = new Buffer(page.content,'utf8'); // <Buffer 68 65 6c 6c 6f>
+         //data.push(page.content)
+  });
+app.get('/api/users', function(req, res) {
+    res.type('.html');  
+  res.send(data);
+});
 
-
-
-
-infinteLoop();
-var j=100;
-function infinteLoop(i){
-    if(i<j){
-       setTimeout(function() {
-           infinteLoop(++i);
-           console.log("tt",i);
-       }, 1);                     
-    }
- 
-}
